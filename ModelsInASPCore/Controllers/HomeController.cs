77b566708b453515aab2655e27e13b9fe2803503
gangs <gangs@DESKTOP-PN82OV3>
2024-08-05 02:42:32 -0700

    Add project files.

---
+++ ModelsInASPCore/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelsInASPCore.Models;
+using ModelsInASPCore.Repository;
+using System.Diagnostics;
+
+namespace ModelsInASPCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly StudentRepository _studentRepository = null;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _studentRepository = new StudentRepository();
+        }
+
+        public List<StudentModel> GetAllStudents()
+        {
+            return _studentRepository.getAllStudents();
+        }
+
+        public StudentModel getByID(int id)
+        {
+            return _studentRepository.getStudentById(id);
+        }
+
+
+        public IActionResult Index()
+        {
+            //var students = new List<StudentModel>
+            //{
+            //    new StudentModel { rollNo = 1,Name = "Kumar",Gender="Male", Standard = 10 },
+            //    new StudentModel { rollNo = 2, Name = "Adil",Gender="Male", Standard = 10 },
+            //    new StudentModel { rollNo = 3, Name = "Natasha",Gender="Female", Standard = 11 }
+            //};
+
+            //ViewData["myStudents"] = students;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ModelsInASPCore/Repository/StudentRepository.cs
@@ -0,0 +1,28 @@
+﻿using ModelsInASPCore.Models;
+
+namespace ModelsInASPCore.Repository
+{
+    public class StudentRepository : IStudent
+    {
+        public List<StudentModel> getAllStudents()
+        {
+            return DataSourse();
+        }
+
+        public StudentModel getStudentById(int id)
+        {
+            return DataSourse().Where(x => x.rollNo == id).FirstOrDefault();
+        }
+        private List<StudentModel> DataSourse()
+        {
+            return new List<StudentModel>
+            {
+                new StudentModel { rollNo = 1,Name = "Kumar",Gender="Male", Standard = 10 },
+                new StudentModel { rollNo = 2, Name = "Adil",Gender="Male", Standard = 10 },
+                new StudentModel { rollNo = 3, Name = "Natasha",Gender="Male", Standard = 11 },
+                new StudentModel { rollNo = 4, Name = "Ahmad",Gender="Female", Standard = 12 },
+                new StudentModel { rollNo = 5, Name = "Ayesha",Gender="Female", Standard = 9 }
+            };
+        }
+    }
+}
